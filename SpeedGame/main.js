7ed24822bc08d84669bb08ddc8e1f37dfefa2ad0
Pavel-Kliukin <9213087@mail.ru>
2023-03-06 03:06:54 +0200

    All main functionality done

--- SpeedGame/main.js
+++ SpeedGame/main.js
@@ -1,62 +1,82 @@
 const circles = document.querySelectorAll('.circles')
 const scoreIs = document.getElementById('scoreIs')
-const startButton = document.querySelector('#start_button')
-const stopButton = document.querySelector('#stop_button')
+const yourScore = document.getElementById('yourScore')
+const startButton = document.querySelector('#startButton')
+const stopButton = document.querySelector('#stopButton')
+const closeButton = document.querySelector('#closeButton')
 let score = 0
 let hlCircleNumber = 0
+let timer
+let pace = 1000
+let missedRounds = 0
 
-function startGame() {
-  startButton.classList.add('notActiveButton')
-  stopButton.classList.remove('notActiveButton')
-  stopButton.classList.add('activeButton')
+function startGame () {
+  startButton.classList.add('hidenButton')
+  stopButton.classList.remove('hidenButton')
   scoreIs.textContent = 0
 
-  highliteCircle()
+  function newRound (hlCircle) {
+    if (missedRounds >= 4) {
+      return stopGame()
+    }
+    timer = setTimeout(highliteCircle, pace)
+    setTimeout(() => hlCircle.classList.remove('highlighted'), pace)
+    pace -= 10
+    missedRounds++
+  }
 
-  function highliteCircle() {
+  function highliteCircle () {
     const randomNumber = Math.floor(Math.random() * 4 + 1)
     if (hlCircleNumber === randomNumber) { // if random number is equival to the number of already highlighted circle
       highliteCircle() // then we choose another random number
     } else {
-      console.log(randomNumber)
       const randomCircle = document.querySelector(`#c${randomNumber}`)
       randomCircle.classList.add('highlighted')
       hlCircleNumber = randomNumber
+      newRound(randomCircle)
     }
   }
+
+  function circleClicked (i) {
+    if (i === hlCircleNumber) {
+      score += 1
+      missedRounds = 0
+      scoreIs.textContent = score
+    } else {
+      stopGame()
+    }
+  }
+
+  highliteCircle()
 
   circles.forEach((circle, i) => {
     circle.addEventListener('click', () => circleClicked(i + 1)) // () - anonimous function that starts another function circleClicked with parametr i
   })
-
-  function circleClicked(i) {
-    const hlCircle = document.querySelector(`#c${hlCircleNumber}`)
-    console.log(`Circle ${i} was clicked`)
-    if (i === hlCircleNumber) {
-      hlCircle.classList.remove('highlighted')
-      highliteCircle()
-      score += 1
-    }
-    scoreIs.textContent = score
-  }
 }
 
-function stopGame() {
+function stopGame () {
   const closeButton = document.querySelector('#closeButton')
-  const body = document.querySelector('body')
   hlCircleNumber = 0 // to cut the possibilty of proceeding game and getting points
+
+  function modalShow () {
+    document.querySelector('.overlay').classList.toggle('visible')
+    yourScore.textContent = `Your score: ${score}`
+  }
 
   for (const circle of circles) {
     circle.classList.remove('highlighted')
   }
 
-  const modalShow = () => {
-    document.querySelector('.overlay').classList.toggle('visible')
-  }
-
   modalShow()
 
-  stopButton.classList.add('notActiveButton')
-  body.style.background = 'linear-gradient(to left bottom, #efcf96, red)'
   closeButton.addEventListener('click', modalShow)
+  clearTimeout(timer)
 }
+
+function resetGame () {
+  window.location.reload()
+}
+
+startButton.addEventListener('click', startGame)
+stopButton.addEventListener('click', stopGame)
+closeButton.addEventListener('click', resetGame)
